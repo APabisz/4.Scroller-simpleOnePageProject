9a19805fbb36778b5aafa464f66ca8772ce9acf7
Arek <arkadiusz.pabisz@gmail.com>
2021-05-28 22:30:26 +0200

    add mobile styles and swiper class for mobile

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scroller = new Scroller("#root")
 
   document.addEventListener("wheel", scroller.listenScroll)
+
+  document.addEventListener("swipeUp", () => scroller.scroll(-1))
+  document.addEventListener("swipeDown", () => scroller.scroll(1))
 })
