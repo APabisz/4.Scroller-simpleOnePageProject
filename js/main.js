bec65edf46a2bae948495dc273cf98890b29bcc8
Arek <arkadiusz.pabisz@gmail.com>
2021-05-28 19:34:28 +0200

    feat: smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded")
-
   const rootElement = document.querySelector("#root")
   const sections = document.querySelectorAll("#root .section")
-  console.log(sections)
+  let currentSectionIndex = 0
+  let isThrottled = false
 
   document.addEventListener("wheel", function (e) {
-    console.log(e.deltaY)
+    if (isThrottled) return
+    isThrottled = true
+
+    setTimeout(() => (isThrottled = false), 800)
+
+    const direction = e.deltaY > 0 ? 1 : -1
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1
+      if (isLastSection) return
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0
+      if (isFirstSection) return
+    }
+
+    currentSectionIndex += direction
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
   })
 })
